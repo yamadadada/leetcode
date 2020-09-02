4ec91f4bc58fd0b4d9c713233dc8665753fdc49d
yamadadada <297354270@qq.com>
2020-09-02 20:28:39 +0800

    20

--- src/Test.java
+++ src/Test.java
@@ -6,8 +6,6 @@ import java.util.List;
 public class Test {
 
     public static void main(String[] args) {
-        Solution solution = new Solution();
-        int[] nums = {1, 5, 2};
-        System.out.println(solution.PredictTheWinner(nums));
+
     }
 }
