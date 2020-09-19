0c310705a5d3211a1153741d63fe2a6d0e9a151e
yamadadada <297354270@qq.com>
2020-09-19 13:21:45 +0800

    9.19

--- src/Solution.java
+++ src/Solution.java
@@ -1377,6 +1377,22 @@ public class Solution {
     }
 
     /**
+     * 404. 左叶子之和
+     * @param root
+     * @return
+     */
+    public int sumOfLeftLeaves(TreeNode root) {
+        if (root == null || (root.left == null && root.right == null)) {
+            return 0;
+        }
+        int sum = 0;
+        if (root.left != null && root.left.left == null && root.left.right == null) {
+            sum += root.left.val;
+        }
+        return sum + sumOfLeftLeaves(root.left) + sumOfLeftLeaves(root.right);
+    }
+
+    /**
      * 443. 压缩字符串
      * @param chars
      * @return
