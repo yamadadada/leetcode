7d83b7c0c6bfea68ac111a28c6f223b0f9fadcc4
yamadadada <297354270@qq.com>
2020-10-15 14:42:15 +0800

    10.15

--- src/Solution.java
+++ src/Solution.java
@@ -1,5 +1,6 @@
 import com.sun.org.apache.bcel.internal.generic.IF_ACMPEQ;
 import common.ListNode;
+import common.Node;
 import common.TreeNode;
 
 import java.math.BigInteger;
@@ -969,6 +970,33 @@ public class Solution {
             root.right = root.left;
             root.left = null;
         }
+    }
+
+    /**
+     * 116. 填充每个节点的下一个右侧节点指针
+     * @param root
+     * @return
+     */
+    public Node connect(Node root) {
+        if (root == null) {
+            return null;
+        }
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                Node node = queue.poll();
+                if (i < size - 1) {
+                    node.next = queue.peek();
+                }
+                if (node.left != null) {
+                    queue.add(node.left);
+                    queue.add(node.right);
+                }
+            }
+        }
+        return root;
     }
 
     /**
