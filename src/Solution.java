7c1af8fa33aaec0359535c7791e1c1f35ddf8f38
yamadadada <297354270@qq.com>
2020-11-26 10:36:40 +0800

    11.26

--- src/Solution.java
+++ src/Solution.java
@@ -1461,6 +1461,46 @@ public class Solution {
     }
 
     /**
+     * 164. 最大间距
+     * @param nums
+     * @return
+     */
+    public int maximumGap(int[] nums) {
+        int n = nums.length;
+        if (n < 2) {
+            return 0;
+        }
+        int[] buf = new int[n];
+        int maxValue = Arrays.stream(nums).max().getAsInt();
+
+        long exp = 1;
+        while (maxValue >= exp) {
+            int[] cnt = new int[10];
+            for (int num : nums) {
+                int digit = (num / (int) exp) % 10;
+                cnt[digit]++;
+            }
+            for (int i = 1; i < 10; i++) {
+                cnt[i] += cnt[i - 1];
+
+            }
+            for (int i = n - 1; i >= 0; i--) {
+                int digit = (nums[i] / (int) exp) % 10;
+                buf[cnt[digit] - 1] = nums[i];
+                cnt[digit]--;
+            }
+            System.arraycopy(buf, 0, nums, 0, n);
+            exp *= 10;
+        }
+
+        int ret = 0;
+        for (int i = 1; i < n; i++) {
+            ret = Math.max(ret, nums[i] - nums[i - 1]);
+        }
+        return ret;
+    }
+
+    /**
      * 167. 两数之和 II - 输入有序数组
      * @param numbers
      * @param target
@@ -3487,7 +3527,7 @@ public class Solution {
     }
 
     /**
-     *
+     * 1370. 上升下降字符串
      * @param s
      * @return
      */
