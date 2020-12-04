a05a11345325245f8953a694196783e0aba11071
yamadadada <297354270@qq.com>
2020-12-04 14:52:50 +0800

    12.4

--- src/Solution.java
+++ src/Solution.java
@@ -2766,6 +2766,42 @@ public class Solution {
     }
 
     /**
+     * 659. 分割数组为连续子序列
+     * @param nums
+     * @return
+     */
+    public boolean isPossible(int[] nums) {
+        Map<Integer, Integer> countMap = new HashMap<>();
+        Map<Integer, Integer> endMap = new HashMap<>();
+        for (int num : nums) {
+            countMap.put(num, countMap.getOrDefault(num, 0) + 1);
+        }
+        for (int num : nums) {
+            int count = countMap.getOrDefault(num, 0);
+            if (count > 0) {
+                int preEnd = endMap.getOrDefault(num - 1, 0);
+                if (preEnd > 0) {
+                    countMap.put(num, count - 1);
+                    endMap.put(num - 1, preEnd - 1);
+                    endMap.put(num, endMap.getOrDefault(num, 0) + 1);
+                } else {
+                    int count1 = countMap.getOrDefault(num + 1, 0);
+                    int count2 = countMap.getOrDefault(num + 2, 0);
+                    if (count1 > 0 && count2 > 0) {
+                        countMap.put(num, count - 1);
+                        countMap.put(num + 1, count1 - 1);
+                        countMap.put(num + 2, count2 - 1);
+                        endMap.put(num + 2, endMap.getOrDefault(num + 2, 0) + 1);
+                    } else {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+    /**
      * 662. 二叉树最大宽度
      * @param root
      * @return
