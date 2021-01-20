c8f4b18247041c786e3a1c723f847169e3a54e2f
yamadadada <297354270@qq.com>
2021-01-20 14:44:24 +0800

    1.20

--- src/Solution.java
+++ src/Solution.java
@@ -3104,6 +3104,39 @@ public class Solution {
     }
 
     /**
+     * 628. 三个数的最大乘积
+     * @param nums
+     * @return
+     */
+    public int maximumProduct(int[] nums) {
+        // 考虑到负数的情况，找出最小的两个数和最大的三个数
+        int min1 = Integer.MAX_VALUE;
+        int min2 = Integer.MAX_VALUE;
+        int max1 = Integer.MIN_VALUE;
+        int max2 = Integer.MIN_VALUE;
+        int max3 = Integer.MIN_VALUE;
+        for (int num : nums) {
+            if (num > max1) {
+                max3 = max2;
+                max2 = max1;
+                max1 = num;
+            } else if (num > max2) {
+                max3 = max2;
+                max2 = num;
+            } else if (num > max3) {
+                max3 = num;
+            }
+            if (num < min1) {
+                min2 = min1;
+                min1 = num;
+            } else if (num < min2) {
+                min2 = num;
+            }
+        }
+        return Math.max(max1 * min1 * min2, max1 * max2 * max3);
+    }
+
+    /**
      * 643. 子数组最大平均数 I
      * @param nums
      * @param k
